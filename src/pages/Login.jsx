ec8efda82d806f4b37e54efaad7360ba11b76d0a
cbiveu <cbiveu@gmail.com>
2024-05-03 22:15:44 -0300

    style: 💄 estilizado a página de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UsersContext } from '../context/UsersContext';
 import logo from '../assets/destinoCerto.png';
+import { FaLock } from 'react-icons/fa6';
+import { MdAlternateEmail } from 'react-icons/md';
 function Login() {
   const { userLogin } = useContext(UsersContext);
   const [user, setUser] = useState({
@@ -14,31 +16,50 @@ function Login() {
   }
 
   return (
-    <>
-      <img src={logo} className='logo' alt='Destino certo' />
-      <form>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          value={user.email}
-          onChange={(event) => setUser({ ...user, email: event.target.value })}
-          id='email'
-        />
-        <label htmlFor='password'>Senha</label>
-        <input
-          type='password'
-          value={user.password}
-          onChange={(event) =>
-            setUser({ ...user, password: event.target.value })
-          }
-          id='password'
-        />
-        <button onClick={() => makeLogin()}>Entrar</button>
-      </form>
-      <div>
-        Não possui uma conta? <Link to='/users/create'>Cadastrar</Link>
+    <div className='login-main'>
+      <div className='container-main'>
+        <div className='header-container'>
+          <div className='logo-container'>
+            <img src={logo} className='logo' alt='Destino certo' />
+          </div>
+        </div>
+        <div className='container-form'>
+          <div className='card-form'>
+            <form>
+              <div className='align-icon'>
+                <MdAlternateEmail /> <label htmlFor='email'>E-mail</label>
+              </div>
+              <input
+                type='email'
+                value={user.email}
+                onChange={(event) =>
+                  setUser({ ...user, email: event.target.value })
+                }
+                id='email'
+              />
+              <div className='align-icon'>
+                <FaLock /> <label htmlFor='password'>Senha</label>
+              </div>
+              <input
+                type='password'
+                value={user.password}
+                onChange={(event) =>
+                  setUser({ ...user, password: event.target.value })
+                }
+                id='password'
+              />
+              <button type='submit' onClick={() => makeLogin()}>
+                Entrar
+              </button>
+            </form>
+            <div className='singin'>
+              Mó Quiridu ainda não tem conta? <br />
+              <Link to='/users/create'>Dázumbanho e te cadastra aqui!</Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
